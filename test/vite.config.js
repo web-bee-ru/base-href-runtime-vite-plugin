a694c90faa00cdccbc7e15ef30340baae135d782
lamtev178 <lamtev2000@gmail.com>
2023-05-25 18:31:28 +0300

    fix: READMI test

---
+++ test/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import baseHrefRuntimeVitePlugin from "..";
+
+export default defineConfig({
+  plugins: [
+    baseHrefRuntimeVitePlugin({
+      fallbackBaseHref: "/", // in case when we didn't match location.pathname
+      publicPaths: [
+        // availabled prefixes. Order is important!
+        "/ui/app/log/", // <base href="/ui/app/log">
+        "/ui/app/", // <base href="/ui/app/">
+        "/ui/test/entrypoint/", // <base href="/ui/test/entrypoint/">
+        "/a/b/c/d/e/", // <base href="/a/b/c/d/e/">
+      ],
+    }),
+  ],
+});
